827e96838ad3fa4dcc446514bcab1c28fda08164
Ghostik226 <91909189+Ghostik226@users.noreply.github.com>
2021-10-11 21:39:54 +0300

    Update anketa1.php

--- anketa1.php
+++ anketa1.php
@@ -1,16 +1,1 @@
-<!DOCTYPE html>
-<html>
-<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-<body>
-<div class="container">
-<div class="top">Данные анкеты получены</div>
-<header class="clearfix">
-<h2>Welcome!!!!<br>
-<?php echo $_POST["fname"]." ".$_POST["surname"]; ?><br>
-Будем рады видеть Вас <br>на выставке робототехники <br>
-
-Дата вашего заезда -<?php echo $_POST["date"]?>
-
-</header>
-</body>
-</html>
+<?php echo СПАСИБО>
