44ee05115d847965e75f31a8f3f8a3eb54f3fd35
Ghostik226 <91909189+Ghostik226@users.noreply.github.com>
2021-10-11 21:50:17 +0300

    Update anketa1.php

--- anketa1.php
+++ anketa1.php
@@ -1,1 +1,8 @@
+<!DOCTYPE html>
+<html>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="main.css" />
+<body>
 <?php echo СПАСИБО?>
+</body>
+</html>
